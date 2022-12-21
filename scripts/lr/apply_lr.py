8f4b685cf59d6b6368672f157333bb9d8b023f40
Rafael Mostert ij <rafaelmostert@gmail.com>
2022-12-21 15:15:08 +0100

    First changes in adapting apply_lr.py to work for single fields

--- scripts/lr/apply_lr.py
+++ scripts/lr/apply_lr.py
@@ -1,5 +1,7 @@
 # %% 
 from glob import glob
+import pandas as pd
+from time import time
 import multiprocessing
 import pickle
 import os
@@ -8,20 +10,10 @@ import numpy as np
 from astropy.coordinates import SkyCoord, search_around_sky
 from astropy.table import Table, join
 from astropy import units as u
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
 
-try:
-    BASEPATH = os.path.dirname(os.path.realpath(__file__))
-    data_path = os.path.join(BASEPATH, "..", "..", "data")
-except NameError:
-    if os.path.exists("data"):
-        BASEPATH = "."
-        data_path = os.path.join(BASEPATH, "data")
-    else:
-        BASEPATH = os.getcwd()
-        data_path = os.path.join(BASEPATH, "..", "..", "data")
-
-sys.path.append(os.path.join(BASEPATH, '..', '..', 'src'))
+BASE_LR_PATH = os.getenv('BASE_LR_PATH')
+sys.path.append(os.path.join(BASE_LR_PATH, 'src'))
 from mltier1 import MultiMLEstimator, parallel_process, get_sigma_all
 
 # LOAD CONFIG from ENV file
@@ -29,16 +21,23 @@ from mltier1 import MultiMLEstimator, parallel_process, get_sigma_all
 #LEGACY_DATA_PATH=/disk02/jsm/Legacy_data-south-13h/Legacy
 #UNWISE_DATA_PATH=/disk02/jsm/Legacy_data-south-13h/unWISE
 #REGION=s13a
-load_dotenv(find_dotenv())
+#load_dotenv(find_dotenv())
 COMBINED_DATA_PATH = os.getenv("COMBINED_DATA_PATH")
 PARAMS_PATH = os.getenv("PARAMS_PATH")
-THRESHOLD = os.getenv("THRESHOLD")
-RADIO_CATALOGUE = os.getenv("RADIO_CATALOGUE")
-OUTPUT_RADIO_CATALOGUE = os.getenv("OUTPUT_RADIO_CATALOGUE")
+THRESHOLD = os.getenv("LR_THRESHOLD")
+RADIO_CATALOGUE = os.getenv("RADIOCAT")
+OUTPUT_RADIO_CATALOGUE = os.getenv("LRCAT")
+FIELD = os.getenv('FIELD')
 
 # Default config parameters
 base_optical_catalogue = COMBINED_DATA_PATH
-params = pickle.load(open(PARAMS_PATH, "rb"))
+hdf_pruned_optical = base_optical_catalogue.replace('.fits',
+        f'_{FIELD}.fits')
+#params = pickle.load(open(PARAMS_PATH, "rb"))
+params = [pd.read_pickle(os.path.join(BASE_LR_PATH,'data/params',f))
+        for f in ['lofar_params-n13c.pckl',
+            'lofar_params-s0a.pckl','lofar_params-s13a.pckl']]
+
 colour_limits = np.array([0.7, 1.2, 1.5, 2. , 2.4, 2.8, 3.1, 3.6, 4.1])
 threshold = float(THRESHOLD)
 max_major = 15
@@ -52,13 +51,35 @@ input_catalogue = RADIO_CATALOGUE
 output_catalogue = OUTPUT_RADIO_CATALOGUE
 
 # %% 
-bin_list, centers, Q_0_colour, n_m, q_m = params
+#bin_list, centers, Q_0_colour, n_m, q_m = params
+centers = np.mean([p[1] for p in params], axis=0)
+Q_0_colour = np.mean([p[2] for p in params], axis=0)
+n_m = np.mean([p[3] for p in params], axis=0)
+q_m = np.mean([p[4] for p in params], axis=0)
+
 
 ## Load the catalogues
-print("Load optical catalogue")
-combined = Table.read(base_optical_catalogue)
 print("Load input catalogue")
-lofar_all = Table.read(input_catalogue)
+lofar= Table.read(input_catalogue)
+print("Load optical catalogue")
+if os.path.exists(hdf_pruned_optical):
+    combined = Table.read(hdf_pruned_optical)
+else:
+    combined = Table.read(base_optical_catalogue)
+    min_ra = np.min(lofar['RA'])-0.01
+    max_ra = np.max(lofar['RA'])+0.01
+    min_dec = np.min(lofar['DEC'])-0.01
+    max_dec = np.max(lofar['DEC'])+0.01
+    start = time()
+    print("Pruning optical cat step 1/4")
+    combined = combined[combined['RA']> min_ra]
+    print(f"Pruning optical cat step 2/4. (Seconds elapsed on prev step: {time()-start:.0f})")
+    combined = combined[combined['RA']< max_ra]
+    print(f"Pruning optical cat step 3/4. (Seconds elapsed on prev step: {time()-start:.0f})")
+    combined = combined[combined['DEC']> min_dec]
+    print(f"Pruning optical cat step 4/4. (Seconds elapsed on prev step: {time()-start:.0f})")
+    combined = combined[combined['DEC']< max_dec]
+    combined.write(hdf_pruned_optical, format="fits")
 
 ## Get the coordinates
 coords_combined = SkyCoord(combined['RA'], 
@@ -174,16 +195,21 @@ combined["lr_index_sel"] = combined_aux_index.astype(float)
 print("Combine catalogues")
 pwl = join(lofar, combined, join_type='left', keys='lr_index_sel')
 print("Clean catalogues")
+print("type of pwl is:", type(pwl))
 for col in pwl.colnames:
-    fv = pwl[col].fill_value
-    if (isinstance(fv, np.float64) and (fv != 1e+20)):
-        print(col, fv)
-        pwl[col].fill_value = 1e+20
+    try:
+        fv = pwl[col].fill_value
+        if (isinstance(fv, np.float64) and (fv != 1e+20)):
+            print(col, fv)
+            pwl[col].fill_value = 1e+20
+        print("Colname is:", col)
+    except:
+        pass
 print("Save output")
 pwl["RA_2"].name = "ra"
 pwl["DEC_2"].name = "dec"
 pwl["RA_1"].name = "RA"
 pwl["DEC_1"].name = "DEC"
-pwl.filled().write(output_catalogue, format="fits")
+pwl.filled().write(output_catalogue, format="fits", overwrite=True)
 
     
